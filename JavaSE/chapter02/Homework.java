4c664f6f8281fc3bb816ae422f3ad45d6ea8ee07
maxsun821 <maxsun8216@gmail.com>
2024-12-23 07:36:59 +0800

    variable

---
+++ JavaSE/chapter02/Homework.java
@@ -0,0 +1,27 @@
+public class Homework {
+	// public static void main(String[] args) {
+	// 	String name1 = "张三";
+	// 	String name2 = "李四";
+	// 	int age1 = 20;
+	// 	int age2 = 30;
+	// 	char gender1 = '男';
+	// 	char gender2 = '女';
+	// 	String tel1 = "12545457585";
+	// 	String tel2 = "15622525855";
+
+	// 	System.out.println("姓名\t年龄\t性别\t联系电话");
+	// 	System.out.println(name1 + "\t" + age1 + "\t" + gender1 + "\t" + tel1);
+	// 	System.out.println(name2 + "\t" + age2 + "\t" + gender2 + "\t" + tel2);
+	// }
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 200;
+
+		int tmp = a;
+		a = b;
+		b = tmp;
+
+		System.out.println(a + "+" + b + "=" + (a + b));
+	}
+}
