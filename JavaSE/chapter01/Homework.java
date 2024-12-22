0284aa6e465d1b78524778cab4890226c9a57df7
maxsun821 <maxsun8216@gmail.com>
2024-12-22 15:22:48 +0800

    java print in screem

---
+++ JavaSE/chapter01/Homework.java
@@ -0,0 +1,33 @@
+public class Homework {
+	// 打印信息
+	// public static void main(String[] args) {
+	// 	System.out.println("姓名：张三");
+	// 	System.out.println("年龄：25");
+	// 	System.out.println("性别：男");
+	// 	System.out.println("家庭住址：北京市海淀区");
+	// 	System.out.println("联系电话：13888888888");
+	// }
+
+	// 打印菱形
+	// public static void main(String[] args) {
+	// 	System.out.println("    *");
+	// 	System.out.println("   ***");
+	// 	System.out.println("  *****");
+	// 	System.out.println(" *******");
+	// 	System.out.println("*********");
+	// 	System.out.println(" *******");
+	// 	System.out.println("  *****");
+	// 	System.out.println("   ***");
+	// 	System.out.println("    *");
+	// }
+
+	// 打印商品列表
+	public static void main(String[] args) {
+		System.out.println("Product\t\tPrice\tStock");
+		System.out.println("Apple\t\t3.5\t10");
+		System.out.println("Banana\t\t2.5\t20");
+		System.out.println("Orange\t\t4.0\t15");
+		System.out.println("Watermelon\t5.0\t5");
+		System.out.println("Grape\t\t6.0\t8");
+	}
+}
